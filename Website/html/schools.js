a43c23513f8a3e7d92adb2e4b49d295267395891
JasonHTD <Jasonhtd11@hotmail.com>
2020-12-06 22:47:45 -0500

    Final Website

---
+++ Website/html/schools.js
@@ -0,0 +1,13 @@
+var interval = setInterval(clock, 1000);
+
+function clock() {
+  var date = new Date();
+  document.getElementById("clck").innerHTML = date.toLocaleTimeString();
+}
+//ABOVE FUNCTION USED FROM https://www.w3schools.com/js/js_timing.asp
+
+function light() {
+   var element = document.body;
+   element.classList.toggle("light-mode");
+}
+//ABOVE FUNCTION USED FROM https://www.w3schools.com/howto/howto_js_toggle_dark_mode.asp
